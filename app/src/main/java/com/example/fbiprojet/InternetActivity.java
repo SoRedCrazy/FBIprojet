5edef553ea76ec484ec0b3b024694cb01739c737
Julien boisgard <julien.boisgard37@gmail.com>
2023-03-07 17:16:15 +0100

    add intent tool bar and add activity

---
+++ app/src/main/java/com/example/fbiprojet/InternetActivity.java
@@ -0,0 +1,15 @@
+package com.example.fbiprojet;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class InternetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internet);
+    }
+}
